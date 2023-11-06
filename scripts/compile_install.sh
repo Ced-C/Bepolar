8739904e9bef72af04c1ce75bfcaac77210a09fa
Ced’C <ced.chanseaulme@gmail.com>
2023-11-06 12:22:29 +0100

    Scritp — Now compiling also provides up-to-date files in the `dist` directory.

--- scripts/compile_install.sh
+++ scripts/compile_install.sh
@@ -11,10 +11,12 @@ sudo xkalamine install Bépolar.yml
 sudo xkalamine install Bépolar2.yml
 
 sudo python3 $path/kalamine_clean.py
+# make version for ditribution `dist` directory
+kalamine $path/../Bépolar.yml
 
 # Copy files in `xkb` directory for quick install
 path_config=/usr/share/X11/xkb
 
-sudo cp $path_config/rules/evdev.xml    $path/../xbk/rules/evdev.xml
-sudo cp $path_config/rules/base.xml     $path/../xbk/rules/base.xml
-sudo cp $path_config/symbols/fr         $path/../xbk/symbols/fr
+sudo cp $path_config/rules/evdev.xml    $path/../xkb/rules/evdev.xml
+sudo cp $path_config/rules/base.xml     $path/../xkb/rules/base.xml
+sudo cp $path_config/symbols/fr         $path/../xkb/symbols/fr
